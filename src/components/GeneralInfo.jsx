106b1283b4ca04b057cb0e2ab44fcfe62fee481b
halfway-there1 <90105000+halfway-there1@users.noreply.github.com>
2023-11-13 23:07:58 +0530

    First commit

---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,17 @@
+// import { usesState } from 'react';
+
+function GeneralInfo() {
+  return (
+    <>
+      <h3>Contact Info</h3>
+      <form>
+        <p>
+          <label htmlFor="name">Name </label>
+          <input type="text" id="name" />
+        </p>
+      </form>
+    </>
+  );
+}
+
+export default GeneralInfo;
